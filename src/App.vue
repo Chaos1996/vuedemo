f5448d38dd3a8afe7b1ece08dd3369857746b0d2
Chao <1164764525@qq.com>
2017-08-31 11:28:20 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+<component-a></component-a>
+    <ul>
+      <li v-for="item in list">{{item.name +'-'+item.price}}</li>
+    </ul>
+    <button v-on:click="addItem">addItem</button>
+
+    <p v-html="hello"></p>
+    <p v-text="hello"></p>
+    {{hello}}
+    {{status ? 'success':'fail'}}
+  </div>
+</template>
+
+<script>
+  import  Vue from 'vue'
+  import  componentA from './components/a.vue'
+  export default {
+    components:{componentA},
+  data(){
+    return {
+      hello: '<span><img>worldddd</span>',
+      num : 1,
+      status :true,
+      list:[
+        {
+          name: 'apple',
+          price: 34444
+        },
+        {
+          name: 'banana',
+          price: 3489
+        }
+      ],
+      objList: {
+        name :'apple',
+        price: 34,
+        color:'red',
+        weight:'75'
+      }
+    }
+  },
+    methods:{
+      addItem () {
+        Vue.set(this.list,1,{
+          name: 'my1ccc',
+          price: "999"
+        })
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/a.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{hello}}
+  </div>
+</template>
+
+
+<script>
+  export  default {
+    data(){
+      return{
+       hello: 'i an component a'
+      }
+    }
+  }
+
+</script>
+
